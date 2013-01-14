37e7f081ffb9a84ae1ea11ac5224f56978833bf8
Felipe Elias Philipp <felipeelias@gmail.com>
2013-01-14 23:46:22 +0100

    Call for load hooks when gem is loaded

--- lib/resubject.rb
+++ lib/resubject.rb
@@ -3,3 +3,5 @@ require 'resubject/naming'
 require 'resubject/presenter'
 require 'resubject/builder'
 require 'resubject/rails' if defined? Rails
+
+ActiveSupport.run_load_hooks(:resubject, Resubject::Presenter)
