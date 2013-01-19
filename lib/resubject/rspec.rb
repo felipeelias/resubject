02a0eb29598407b96b9197ce9bf68164c88ad1e8
Felipe Elias Philipp <felipeelias@gmail.com>
2013-01-19 12:54:07 +0100

    Add docs for RSpec extension

--- lib/resubject/rspec.rb
+++ lib/resubject/rspec.rb
@@ -1,7 +1,32 @@
 require 'resubject'
 
 module Resubject
-  module RspecHelpers
+  # RSpec configuration and helpers
+  #
+  # This helper automatically creates `subject` and `template`
+  # variables for RSpec when testing your presenter.
+  #
+  # To get this working, create a spec file in `spec/presenters`
+  # and require `resubject/rspec`. You only need to define the
+  # object under test by creating a `let(:object)`, like so:
+  #
+  # @example Isolated spec for MyPresenter
+  #
+  #   # spec/presenters/my_presenter_spec.rb
+  #   require 'resubject/rspec'
+  #   require 'presenters/my_presenter'
+  #
+  #   describe MyPresenter do
+  #     let(:object) { mock :presented }
+  #
+  #     it 'has full name' do
+  #       object.stub(first: 'My', last: 'Name')
+  #       expect(subject.name).to eq 'My Name'
+  #     end
+  #   end
+  #
+  module Rspec
+    # Extend RSpec configuration for files placed in `spec/presenters`
     def self.included(base)
       base.instance_eval do
         let(:template) do
@@ -17,11 +42,11 @@ module Resubject
         end
       end
     end
-
-    RSpec.configure do |config|
-      config.include self,
-                     :type => :presenter,
-                     :example_group => { :file_path => %r(spec/presenters) }
-    end
   end
+end
+
+::RSpec.configure do |c|
+  c.include Resubject::Rspec,
+            type: :presenter,
+            example_group: { file_path: %r(spec/presenters) }
 end
